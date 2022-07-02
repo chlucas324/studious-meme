e3f540cde6d40e0e0183e631fbda2352467cf6ea
chlucas324 <chlucas324@gmail.com>
2022-07-01 20:41:26 -0400

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,21 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/index.html",
-    "/css/styles.css",
-    "/favicon.ico"
+    "./index.html",
+    "./css/styles.css",
+    "./favicon.ico",
+    "./js/idb.js",
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon384x384.png",
+    "./icons/icon-512x512.png"
 ];
 
 // Install the service worker
